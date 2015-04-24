92f721857b9b67a6de0e2ea2927f00102fc2829a
DustJ <dustin@dustinjacobson.com>
2015-04-24 08:35:42 -0600

    Revert "Initial Commit"
    This reverts commit e3e0463501a3239b10c5d968f3763cc369f8a14d.

--- out/production/CordovaLib/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \

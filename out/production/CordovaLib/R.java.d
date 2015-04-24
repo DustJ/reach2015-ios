59b602578756e4874f5e86a571971c70e610adb8
DustJ <dustin@dustinjacobson.com>
2015-04-24 08:50:51 -0600

    Cleaned up directories

---
+++ out/production/CordovaLib/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ out/production/android/R.java.d
@@ -0,0 +1,25 @@
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/ant-gen/com/vitalsmarts/event/testapp/R.java \
+ : /Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable/icon.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-hdpi/ic_action_next_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-hdpi/ic_action_previous_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-hdpi/ic_action_remove.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-land-hdpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-land-ldpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-land-mdpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-land-xhdpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-mdpi/ic_action_next_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-mdpi/ic_action_previous_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-mdpi/ic_action_remove.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-port-hdpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-port-ldpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-port-mdpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-port-xhdpi/screen.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-xhdpi/ic_action_next_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-xhdpi/ic_action_previous_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-xhdpi/ic_action_remove.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-xxhdpi/ic_action_next_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-xxhdpi/ic_action_previous_item.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/drawable-xxhdpi/ic_action_remove.png \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/values/strings.xml \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/res/xml/config.xml \
+/Users/Dust/Desktop/Projects/Reach 2015 - DPS App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach 2015 - Test/platforms/android/ant-build/AndroidManifest.xml \

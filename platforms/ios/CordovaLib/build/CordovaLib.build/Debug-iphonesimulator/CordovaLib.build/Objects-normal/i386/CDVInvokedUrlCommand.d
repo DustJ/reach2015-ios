1297feb9f93375d81c72d1813a5896bec8ffb6e0
DustJ <dustin@dustinjacobson.com>
2017-12-28 09:41:10 -0700

    cleaning up unnecessary files and folders

--- platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/CDVInvokedUrlCommand.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVInvokedUrlCommand.m \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVInvokedUrlCommand.h \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVJSON_private.h \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/NSData+Base64.h \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVAvailabilityDeprecated.h
--- platforms/ios/CordovaLib/build/CordovaLib.build/Debug-iphonesimulator/CordovaLib.build/Objects-normal/i386/CDVWebViewDelegate.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVWebViewDelegate.m \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVWebViewDelegate.h \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVAvailability.h \
-  /Users/Dust/Desktop/Projects/Reach\ 2015\ -\ DPS\ App/Working/Reach-Proto/framework7/bower_components/phonegap/Reach\ 2015/reach2015-ios/platforms/ios/CordovaLib/Classes/CDVAvailabilityDeprecated.h
